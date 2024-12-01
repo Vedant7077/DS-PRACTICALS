40165c675f6dc679d221f2d83e5ff96635a21699
Vedant7077 <vedantpalekar21@gmail.com>
2024-12-01 22:08:34 +0530

    final commit

--- singly_linked_list.c
+++ singly_linked_list.c
@@ -39,6 +39,34 @@ void insertAtEnd(struct Node **head, int data){
     printf("Elmement %d Inserted\n",data);
 }
 
+void insertAfter(struct Node **head,int data,int x){
+    struct Node* newNode = createNode(data);
+    struct Node* temp = *head;
+    while(temp->next!=NULL){
+        if(temp->data == x){
+            newNode->next = temp->next;
+            temp->next = newNode;
+        }
+        temp = temp->next;
+    }
+    printf("Element %d inserted after a given %d",data,x);
+}
+
+void insertBefore(struct Node **head,int data,int x){
+    struct Node* newNode = createNode(data);
+    struct Node* temp = *head;
+    struct Node* temp2 = NULL;
+    while(temp->next!=NULL){
+        if(temp->data == x){
+            temp2 ->next = newNode;
+            newNode->next = temp;
+        }
+        temp2 = temp;
+        temp = temp->next;
+    }
+    printf("Element %d inserted after a given %d",data,x);
+}
+
 void deleteFromBeginning(struct Node **head){
     if(*head == NULL){
         printf("List is Empty!!");
@@ -51,6 +79,24 @@ void deleteFromBeginning(struct Node **head){
     printf("Elmement Deleted\n");
 }
 
+void deleteFromEnd(struct Node **head){
+    if(*head == NULL){
+        printf("List is Empty!!");
+    }
+    else{
+    struct Node *temp = *head;
+    struct Node *prev = NULL;
+    while(temp->next!=NULL){
+        prev = temp;
+        temp = temp->next;
+    }
+    prev->next = NULL;
+    free(temp);
+    }
+    printf("Elmement Deleted\n");
+}
+
+
 void traverse(struct Node **head){
     struct Node *temp = *head;
     while(temp!=NULL){
@@ -62,9 +108,9 @@ void traverse(struct Node **head){
 
 int main(){
     struct Node *head = NULL;
-    int ch,x;
+    int ch,x,y;
     while(1){
-    printf("\n1.InsertatBeg\n2.InsertatEnd\n3.Delete\n4.display\n5.Exit\n");
+    printf("\n1.InsertatBeg\n2.InsertatEnd\n3.DeleteBefore\n4.display\n5.Exit\n6.insertAfter\n7.insertBefore\n8.DeleteEnd\n");
     printf("Enter your choice:");
     scanf("%d",&ch);
     switch(ch){
@@ -79,6 +125,21 @@ int main(){
         case 3: deleteFromBeginning(&head); break;
         case 4 : traverse(&head); break;
         case 5: exit(0);
+        case 6 :
+                printf("Enter elemnt to be inserted:");
+                scanf("%d",&x);
+                printf("Enter the position:");
+                scanf("%d",&y);
+                insertAfter(&head,x,y);
+                break;
+        case 7:
+                printf("Enter elemnt to be inserted:");
+                scanf("%d",&x);
+                printf("Enter the position:");
+                scanf("%d",&y);
+                insertBefore(&head,x,y);
+                break;
+        case 8: deleteFromEnd(&head);break;
         default: printf("Invalid choice");
     }
     }
